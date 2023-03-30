6e777b9856abcdfb3146b70db3f4cea864c02833
Jakkula <S560445@nwmissouri.edu>
2023-03-29 20:58:04 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -4,18 +4,18 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 if(Object.keys(req.query).length == 0){
-  var h = Math.random();
-  res.render('computation',{val1:`math.cos() applied to ${h} is ${Math.cos(h)}`,
-  val2:`math.asin() applied to ${h} is ${Math.asin(h)}`,
-  val3:`math.asinh() applied to ${h} is ${Math.asinh(h)}`
+  var x = Math.random();
+  res.render('computation',{val1:`math.cos() applied to ${x} is ${Math.cos(x)}`,
+  val2:`math.asin() applied to ${x} is ${Math.asin(x)}`,
+  val3:`math.asinh() applied to ${x} is ${Math.asinh(x)}`
 });
 }
 
 else{
-  for(let k in req.query){
-    res.render('computation',{val1:`math.acosh() applied to ${req.query[k]} is  ${Math.acosh(req.query[k])}`,
-    val2:`math.asin() applied to ${req.query[k]} is ${Math.asin(req.query[k])}`,
-    val3:`math.asinh() applied to ${req.query[k]} is ${Math.asinh(req.query[k])}`})
+  for(let y in req.query){
+    res.render('computation',{val1:`math.acosh() applied to ${req.query[y]} is  ${Math.acosh(req.query[y])}`,
+    val2:`math.asin() applied to ${req.query[y]} is ${Math.asin(req.query[y])}`,
+    val3:`math.asinh() applied to ${req.query[y]} is ${Math.asinh(req.query[y])}`})
   }
 
 }
